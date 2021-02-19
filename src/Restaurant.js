ecb76e31a58546abd1f8f6ee127f1d1bdfd5e2c8
DominikDolphin <dominik.thibaudeau@gmail.com>
2021-02-19 00:53:27 -0500

    restaurant route now displays map and grades of restaurant. Also handles faulty routes and loading

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
-import { Card } from 'react-bootstrap';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Card, CardGroup } from 'react-bootstrap';
 
 /*
 outputs: <p>Restaurants query: query</p> where query is a value that will be available in "props"
@@ -8,7 +8,7 @@ thanks to our routing configuration (see: Step 4)
 
 */
 function Restaurant(props) {
-  const [restaurant, setRestaurant] = useState(null);
+  const [restaurant, setRestaurant] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -19,12 +19,13 @@ function Restaurant(props) {
       })
       .then(function (data) {
         setLoading(false);
-        if (data.hasOwnProperty("_id")) {
 
+        if (data.hasOwnProperty("_id")) {
+          console.log("yay?")
           setRestaurant(data);
           console.log(restaurant);
         } else {
-          
+          console.log("rip");
           setRestaurant(null);
         }
 
@@ -36,18 +37,82 @@ function Restaurant(props) {
       })
 
   }, []);
-  return (
-    
-    <Card >
-      <Card.Body>
-        <Card.Title>{restaurant.name}</Card.Title>
-        <Card.Text>
-          More to come...
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+
+  if (!restaurant && loading == false) {
+    return (
+      <>
+      <br />
+      <Card >
+        <Card.Header>Error</Card.Header>
+        <Card.Body>
+        <Card.Text> Unable to find Restaurant with id: {props.id} </Card.Text>
+        </Card.Body>
+        </Card>
+      </>
+    )
+  } else if (!restaurant && loading== true){
+    return (
+      <>
+      <Card >
+        <Card.Header>Loading</Card.Header>
+        <Card.Body>
+        <Card.Text> Loading Restaurant Data </Card.Text>
+        </Card.Body>
+        </Card>
+      </>
+    )
+  } 
+  else {
+    const position = [restaurant.address.coord[1], restaurant.address.coord[0]];
+    return (
+      <>
+        <Card >
+
+          <Card.Body>
+            <Card.Title>{restaurant.name}</Card.Title>
+            <Card.Text>
+              {restaurant.address.building} {restaurant.address.street}
+            </Card.Text>
+          </Card.Body>
+
+        </Card>
+        <br />
+        <MapContainer style={{ "height": "400px" }} center={position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+          </Marker>
+        </MapContainer>
+        <br />
+        <h2>Ratings</h2>
+        <br />
+        <CardGroup>
+        
+          {restaurant.grades.map((field)=>
+            <Card>
+              <Card.Header>Grade: {field.grade}</Card.Header>
+            <Card.Body>
+              
+              <Card.Text>
+                Completed: {(field.date).substring(0,10)}
+              </Card.Text>
+            </Card.Body>
+            
+          </Card>
+            )}
+          
+         
+        </CardGroup>
+      </>
+    );
+  }
 }
+
 
 export default Restaurant;
 
